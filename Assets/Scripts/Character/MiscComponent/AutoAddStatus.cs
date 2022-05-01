d21225dea261ddb82ab52bd34a7aca50fabd6ae7
Nam <nam.mirbeau@gmail.com>
2022-05-01 10:32:46 +0200

    Sprites Lemina & attack de base

---
+++ Assets/Scripts/Character/MiscComponent/AutoAddStatus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace VoiceActing
+{
+
+    public class AutoAddStatus : MonoBehaviour
+    {
+        [SerializeField]
+        CharacterStatusController statusController = null;
+        [SerializeField]
+        StatusEffectData[] status = null;
+
+        private void Start()
+        {
+            for (int i = 0; i < status.Length; i++)
+            {
+                statusController.ApplyStatus(status[i], 1000);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/StatusComponent/StatusEffectControllers/StatusEffectLemina.cs
@@ -0,0 +1,153 @@
+﻿/*****************************************************************
+ * Product:    #PROJECTNAME#
+ * Developer:  #DEVELOPERNAME#
+ * Company:    #COMPANY#
+ * Date:       #CREATIONDATE#
+******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace VoiceActing
+{
+    [System.Serializable]
+    public class StatusEffectLemina : StatusEffect
+    {
+        #region Attributes 
+
+        /* ======================================== *\
+         *               ATTRIBUTES                 *
+        \* ======================================== */
+
+        [SerializeField]
+        public CardBreakController CardBreakController = null;
+        [SerializeField]
+        public GaugeDrawer GaugeInstall = null;
+        [SerializeField]
+        public CardData AttackInstall = null;
+
+
+        int Meter = 99;
+        int MaxMeter = 99;
+
+        CharacterBase owner = null;
+
+        GaugeDrawer gaugeMeter = null;
+        bool action = false;
+        bool disappear = false;
+        #endregion
+
+        #region GettersSetters 
+
+        /* ======================================== *\
+         *           GETTERS AND SETTERS            *
+        \* ======================================== */
+
+        #endregion
+
+        #region Functions 
+
+        /* ======================================== *\
+         *                FUNCTIONS                 *
+        \* ======================================== */
+        public StatusEffectLemina()
+        {
+
+        }
+        public StatusEffectLemina(StatusEffectLemina data)
+        {
+            CardBreakController = data.CardBreakController;
+            GaugeInstall = data.GaugeInstall;
+            AttackInstall = data.AttackInstall;
+        }
+
+        public override StatusEffect Copy()
+        {
+            return new StatusEffectLemina(this);
+        }
+
+        public override void ApplyEffect(CharacterBase character)
+        {
+            disappear = false;
+            owner = character;
+            character.OnBattleStart += InitializeInstall;
+            character.OnBattleEnd += ResetInstall;
+            CardBreakController.OnCardBreak += CardBreakInstall;
+
+            gaugeMeter = character.CreateAnimation(GaugeInstall.gameObject).GetComponent<GaugeDrawer>();
+
+            InitializeInstall();
+        }
+
+        void InitializeInstall()
+        {
+            Meter = 0;
+            MaxMeter = owner.DeckController.DeckData.Count;
+            gaugeMeter.DrawGauge(Meter, MaxMeter);
+        }
+
+        void ResetInstall()
+        {
+
+        }
+
+        public void CardBreakInstall(CharacterBase characterBreaked, List<Card> cardBreaked, CharacterBase characterBreaker, List<Card> cardBreaker)
+        {
+            if (cardBreaker == null)
+                return;
+            if (characterBreaker == owner) // si le perso qui break est le notre
+            {
+                int sumActive = 0;
+                int sumNewCards = 0;
+
+                for (int i = 0; i < cardBreaked.Count; i++)
+                    sumActive += cardBreaked[i].GetCardValue();
+                for (int i = 0; i < cardBreaker.Count; i++)
+                    sumNewCards += cardBreaker[i].GetCardValue();
+
+                int difference = sumNewCards - sumActive;
+
+                Meter += Mathf.Abs(difference);
+
+                if(Meter >= MaxMeter) // On entre en install
+                {
+                    action = true;
+                    cardBreaker.Clear();
+                    cardBreaker.Add(new Card(AttackInstall, 0));
+                    Meter = 0;
+                }
+
+                // On dessine
+                gaugeMeter.DrawGauge(Meter, MaxMeter);
+            }
+
+        }
+
+        public override void UpdateEffect(CharacterBase character)
+        {
+            base.UpdateEffect(character);
+            /*if(action)
+            {
+                enemy.CharacterAction.Action(new Card(AttackDoom, 6));
+                action = false; 
+                CardBreakController.OnCardBreak -= DoomDecrement;
+                GameObject.Destroy(doomCounter.gameObject);
+                disappear = true;
+            }*/
+        }
+
+        public override void RemoveEffect(CharacterBase character)
+        {
+            character.OnBattleStart -= InitializeInstall;
+            character.OnBattleEnd -= ResetInstall;
+            CardBreakController.OnCardBreak -= CardBreakInstall;
+
+        }
+
+        #endregion
+
+    }
+
+} // #PROJECTNAME# namespace
